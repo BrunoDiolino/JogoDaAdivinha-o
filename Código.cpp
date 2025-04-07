da7ce1b87c6fe51c7a2ce548c58b2880894d168a
Bruno Diolino <brunoemanuel1pereira@gmail.com>
2025-04-07 19:18:59 -0300

    Add files via upload

--- Código.cpp
+++ Código.cpp
@@ -1,25 +1,45 @@
-#include <iostream>;
-#include <tchar.h>;
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main() {
-	_tsetlocale(LC_ALL, _T("portuguese"));
 
 	cout << "***********************************************" << endl;
 	cout << "     Bem-Vindo ao Jogo Da Adivinhação  " << endl;
 	cout << "***********************************************" << endl;
 	
+	cout << "Escolha o nivel de dificuldade:" << endl;
+	cout << "Fácil (F)" << endl << "Médiano (M)" << endl << "Difícil (D)" << endl;
+
+	char dif;
+	cin >> dif;
+	
+	int Num_Tentativas;
+	
+	if (dif == 'F') {
+		Num_Tentativas = 15;
+	}
+	else if (dif == 'M') {
+		Num_Tentativas = 10;
+	}
+	else {
+		cout << "Boa-Sorte :)" << endl;
+		Num_Tentativas = 5;
+	}
+
 	    int tentativas = 0;
 		bool n_acertou = true;
 		double pontos = 1000.0;
 
 
-	while (n_acertou) {
+		for (tentativas = 1;tentativas <= Num_Tentativas;tentativas++);
 		
-		tentativas++;
-		
-		const int NUM_SECRETO = 42;
+		srand(time(NULL)); //para implementar 'rand' primeiro é necessário ,implementar as biliotecas c, implementar a raiz dela 
+		const int NUM_SECRETO = rand(); // o numero não é necessariamente aletório, ele passa previamente por um processo que o dev escolhe
+
+		cout << "Numero sercreto:"<< NUM_SECRETO << endl;
 
 		int chute;
 		cout << "Tentativa: " << tentativas << endl;
@@ -39,6 +59,7 @@ int main() {
 			cout << "Parabéns você acertou o chute!!!" << endl;
 			
 			n_acertou = false;
+			break;
 		}
 		else if (maior) {
 			cout << "Seu chute foi maior que o número sercreto! " << endl;
@@ -47,12 +68,15 @@ int main() {
 			cout << "Seu chute foi menor que o número secreto" << endl;
 		}
 
+	if (n_acertou) {
+		cout << "Você perdeu!Tente novamente!" << endl;
 	}
-	cout << "Você fez:" << tentativas << " tentativas" << endl;
-	cout.precision(2); //para adicionar duas casas ou mais antes da virgula assim como usado para imprimir o valor da mesma forma quando se trata de dinheiro usamos o comando na frente do "cout" .precision(...) (lembrando que se faz isso uma linha antes da linha que se quer adicionar esse comando) 
-	cout << fixed;
-	cout << "Você fez:" << pontos << " pontos" << endl;
-	cout << "fim de jogo" << endl;
-	
+	else {
+		cout << "Você acertou em:" << tentativas << " tentativas" << endl;
+		cout.precision(2); //para adicionar duas casas ou mais antes da virgula assim como usado para imprimir o valor da mesma forma quando se trata de dinheiro usamos o comando na frente do "cout" .precision(...) (lembrando que se faz isso uma linha antes da linha que se quer adicionar esse comando) 
+		cout << fixed;
+		cout << "Você fez:" << pontos << " pontos" << endl;
+		cout << "fim de jogo" << endl;
+	}
 
 }
